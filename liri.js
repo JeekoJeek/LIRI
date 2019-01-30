6e9f8dbd9db6fb2859f211121e6c1f3ac127f11a
John Hansberry <johnvhansberry@gmail.com>
2019-01-30 00:24:19 -0500

    working omdb search

--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify)
 
 var queryURL = ""
+var spacer= "\n~~~~~~~~~~~~~~~~ New Search ~~~~~~~~~~~~~~~~\n"
 var search = process.argv.splice(3).join(" ")
 if (process.argv[2] === "spotify-this-song") {
     if (search===""){
@@ -20,15 +21,32 @@ if (process.argv[2] === "spotify-this-song") {
                 "Album: " + response.tracks.items[0].album.name,
                 "Preview: " + response.tracks.items[0].preview_url
             ].join("\n\n")
-            console.log(songInfo + "\n")
+            console.log(songInfo + spacer)
         })
         .catch(function(err) {
             console.log(err);
           });
 }
 else if(process.argv[2] === "movie-this"){
-    
+    if (search === undefined){
+        search === "Mr. Nobody"
+    }
+    queryURL= "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy"
+    axios.get(queryURL).then(function(result){
+        console.log(result.data)
+        var movie=[
+            "Title: " + result.data.Title,
+            "Year: " + result.data.released,
+            "IMDB Rating: " + result.data.imdbRating,
+            "Rotten Tomatoes: " + result.data.Ratings[1].Value,
+            "Country: " + result.data.Country,
+            "Language: " + result.data.Language,
+            "Plot: " + result.data.Plot,
+            "Actors: " + result.data.Actors
+        ].join("\n\n")
+        console.log(movie + spacer)
+    })
 }
 
 
-// axios.get(queryURL)
+
